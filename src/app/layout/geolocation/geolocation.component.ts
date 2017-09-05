e091ed7bd9cfe68c024fdd16f2d54fdf6c12c6da
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2017-09-05 19:34:09 -0300

    point plotted on the map using geojson format

--- src/app/layout/geolocation/geolocation.component.ts
+++ src/app/layout/geolocation/geolocation.component.ts
@@ -3,7 +3,7 @@ import { SchoolService } from '../../school.service';
 import { routerTransition } from '../../router.animations';
 import {AgmMap} from '@agm/core';
 import * as L from 'leaflet';
-import {Layer} from 'leaflet';
+import {icon, Layer} from 'leaflet';
 
 @Component({
   selector: 'app-geolocation',
@@ -12,6 +12,7 @@ import {Layer} from 'leaflet';
   animations: [routerTransition()]
 })
 export class GeolocationComponent implements OnInit {
+  customDefault: any;
   lat = -23.552133;
   lng = -46.6331418;
   schoolsCoordinates: any;
@@ -22,11 +23,56 @@ export class GeolocationComponent implements OnInit {
   neighboringSchoolsLayer: any;
   allSchoolsLayer: any;
   options2: any;
+  featureCollection: GeoJSON.FeatureCollection<any> = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [this.lng, this.lat]
+        },
+        properties: {}
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-47.14554500000313, -23.98571699999825]
+        },
+        properties: {}
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-47.14556000000313, -23.98568499999824]
+        },
+        properties: {}
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-47.146239000003135, -23.983872999998237]
+        },
+        properties: {}
+      }
+    ]
+  };
+  schoolsCollection: GeoJSON.FeatureCollection<any> = {type: 'FeatureCollection', features: []};
   locationGeoJSON = {
     id: 'geoJSON',
     name: 'GEO JSON Point',
     enabled: true,
-    layer: L.geoJSON()
+    layer: L.geoJSON(this.featureCollection)
+    /*layer: L.geoJSON(
+      ([
+        {type: 'Point', coordinates: [-47.14398600000312, -23.986885999998258]},
+        {type: 'Point', coordinates: [-47.14554500000313, -23.98571699999825]},
+        {type: 'Point', coordinates : [ -47.14556000000313, -23.98568499999824 ] }
+      ]) as any,
+      { style: () => ({ color: '#ff7800' })})*/
   };
   LAYER_OSM = {
     id: 'openstreetmap',
@@ -38,10 +84,14 @@ export class GeolocationComponent implements OnInit {
     })
   };
   // Values to bind to leaflet Directive
+  // layers: L.Layer[];
   layers: L.Layer[];
   layersControl = {
     baseLayers: {
       'Open Street Map': this.LAYER_OSM.layer
+    },
+    overlays: {
+      'Schools': this.allSchoolsLayer
     }
   };
   options = {
@@ -50,21 +100,26 @@ export class GeolocationComponent implements OnInit {
   };
 
   constructor(
-    private schoolService: SchoolService
+    private schoolService: SchoolService,
   ) { }
 
   ngOnInit( ) {
     this.getSchoolsList();
+    this.customDefault = L.icon({
+      iconUrl: 'assets/marker-icon.png',
+      shadowUrl: 'assets/marker-shadow.png'
+    });
 
     this.options2 = {
       layers: [
-        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' })
       ],
       zoom: 14,
       center: L.latLng([this.lat, this.lng])
     };
-    const dom: any = document.querySelector('body');
-    this.allSchoolsLayer = {
+    this.schoolsCollection.features = this.schoolsCoordinates;
+    // const dom: any = document.querySelector('body');
+    /*this.allSchoolsLayer = {
       id: 'geoJSON',
       name: 'GEO JSON Point',
       enabled: true,
@@ -73,9 +128,20 @@ export class GeolocationComponent implements OnInit {
           return L.marker(latlng);
         }
       }).addTo(dom.mymap)
-    };
-
+    };*/
+    /*this.layers = [
+        L.marker([ this.lat, this.lng ], {icon: this.selectedSchoolMarkerIcon }),
+        L.marker([-23.986885999998258, -47.14398600000312], {icon: this.schoolMarkerIcon})
+    ];*/
+    this.layers = [L.geoJSON(this.featureCollection, {
+      pointToLayer: function(feature, latlng) {
+        console.log(latlng, feature);
+        return L.marker(latlng, {icon:L.icon({iconUrl: 'assets/images/marcador_small.png'})});
+      }
+     })
+    ];
   }
+
 
   private geolocateAllSchools () {
     return 0;
@@ -83,7 +149,7 @@ export class GeolocationComponent implements OnInit {
 
   private drawSchoolNeighborhoodArea(neighborhoodRadius: number) {
     this.neighboringSchoolsLayer = [L.circle([ this.lat, this.lng ], { radius: neighborhoodRadius }),
-      L.marker([ this.lat, this.lng ], {icon: this.schoolMarkerIcon })
+      L.marker([ this.lat, this.lng ], {icon: this.selectedSchoolMarkerIcon })
     ];
   }
 
